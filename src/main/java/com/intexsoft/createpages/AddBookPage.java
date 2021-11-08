ffc929f4d29a8fb8f48ce61eed39ff7ac53715eb
Pavel Lotashinski <pavel.lotashinski@intexsoft.by>
2021-11-08 17:07:21 +0300

    add class AddBookPage.java

--- src/main/java/com/intexsoft/createpages/AddBookPage.java
+++ src/main/java/com/intexsoft/createpages/AddBookPage.java
@@ -16,6 +16,8 @@ import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 
+import java.util.ArrayList;
+
 public class AddBookPage extends WebPage {
 
     public AddBookPage() {
@@ -34,7 +36,18 @@ public class AddBookPage extends WebPage {
         TextField<String> descriptionTextField = new TextField<>("description");
         bookForm.add(descriptionTextField);
 
+        compoundPropertyModel.getObject().setAuthorList(new ArrayList<>());
         CheckGroup<Author> authorCheckGroup = new CheckGroup<>("authors");
+        ListView<Author> checksList = new ListView<>("authorsForCheck", AuthorRepository.getInstance().getAll()) {
+            @Override
+            protected void populateItem(ListItem<Author> item) {
+                Check<Author> check = new Check<>("check", item.getModel());
+                check.setLabel(new Model<>(item.getModel().getObject().getName()));
+                item.add(check);
+                item.add(new SimpleFormComponentLabel("author12", check));
+            }
+        }.setReuseItems(true);
+        authorCheckGroup.add(checksList);
         bookForm.add(authorCheckGroup);
 
         DropDownChoice<Publisher> publisherDropDownChoice = new DropDownChoice<>("publisher");
