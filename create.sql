cc6cd63e7c3c5ae9e2983089380ea93864c3851e
Pavel Lotashinski <pavel.lotashinski@intexsoft.by>
2021-11-11 11:10:32 +0300

    Add create.sql

---
+++ create.sql
@@ -0,0 +1,55 @@
+create table author
+(
+    id         uuid         not null
+        constraint author_pkey
+            primary key,
+    date_born  timestamp    not null,
+    date_death timestamp,
+    name       varchar(255) not null,
+    surname    varchar(255) not null
+);
+
+alter table author
+    owner to pavel;
+
+create table publisher
+(
+    id      uuid         not null
+        constraint publisher_pkey
+            primary key,
+    address varchar(255) not null,
+    name    varchar(255) not null,
+    number  varchar(255) not null
+);
+
+alter table publisher
+    owner to pavel;
+
+create table book
+(
+    id           uuid         not null
+        constraint book_pkey
+            primary key,
+    description  varchar(255),
+    name         varchar(255) not null,
+    publisher_id uuid
+        constraint fkgtvt7p649s4x80y6f4842pnfq
+            references publisher
+);
+
+alter table book
+    owner to pavel;
+
+create table author_to_book
+(
+    book_id   uuid not null
+        constraint fksh9c4nxabn353n7sd4miv5yv3
+            references book,
+    author_id uuid not null
+        constraint fk2xxuj4m0iu8qv9jux3xnm3etd
+            references author
+);
+
+alter table author_to_book
+    owner to pavel;
+
